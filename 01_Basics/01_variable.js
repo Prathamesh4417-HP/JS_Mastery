ebe52d6717a69b99c8b659d2adeca43e873002d1
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-09-16 15:45:35 +0000

    understanding variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "prathamesh131@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+let accountState;
+
+accountEmail = "hcf@gmail.com"
+accountPassword = "4674385"
+accountCity = "Banglore"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+// you can print the output like the above method 
+
+// OR
+
+console.table([accountEmail, accountPassword, accountCity,accountState]);
