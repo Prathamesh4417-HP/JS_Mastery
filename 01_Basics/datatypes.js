a56930cbbf4ebb1a94ac3b64853c03376401e895
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-09-23 10:47:23 +0000

    Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// number = 2 to power 52
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined 
+// symbol = unique
+
+// null = datatype of null is object in JavaScript
+
+console.log(typeof null);
+console.log(typeof undefined);
