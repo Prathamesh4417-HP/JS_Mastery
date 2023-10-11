842e2ff8d49126c8a25bccf44032b08905d96c0c
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-10-11 15:50:40 +0000

    arrays

---
+++ JavaScript/arrays.js
@@ -0,0 +1,16 @@
+const myArr = [0,1,2,3,4,5,6]
+const bodyBuilders = ["Ronnie", "jay", "kai", "Rammy"] 
+
+// another method
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+console.log(myArr);
+
+// Array Method
+
+myArr.push(7)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
