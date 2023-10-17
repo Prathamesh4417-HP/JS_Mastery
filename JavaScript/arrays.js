a61dc7c881bc94054e846c3aba391bf0c2e6b160
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-10-17 15:34:35 +0000

    commit

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -14,3 +14,46 @@ console.log(myArr);
 myArr.pop()
 console.log(myArr);
 
+myArr.unshift(9)
+
+console.log(myArr); 
+ // nine will get added at starting of array
+//  [
+//     9, 0, 1, 2,
+//     3, 4, 5, 6
+//   ]
+
+
+myArr.shift()
+console.log(myArr); // delete first element
+
+console.log(myArr.includes(9)); // false
+console.log(myArr.indexOf(4));
+console.log(myArr);
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr); //string
+
+// slice or splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ", myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
