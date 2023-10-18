561d6dae99afac72bdb6be961c6718ad4ae6f69d
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-10-18 17:02:32 +0000

    objects

---
+++ JavaScript/Objects.js
@@ -0,0 +1,21 @@
+// Singleton
+
+// object literals
+
+// constructor method 
+//Object.create
+
+// object literals
+const JsUser = {
+    name: "Prathamesh",
+    age: 20,
+    "number" : 10,
+    location: "Pune",
+    email:"prathameshsonawane@google.com",
+    isLoggedIn : false,
+    LastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser["name", "email"]);
+console.log(JsUser.number);
+
