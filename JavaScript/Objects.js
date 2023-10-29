0ab6a9b9cd1e4e14b1bcbe57d7290ada6047be41
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-10-29 06:36:47 +0000

    obj

--- JavaScript/Objects.js
+++ JavaScript/Objects.js
@@ -6,10 +6,12 @@
 //Object.create
 
 // object literals
+const mySym = Symbol("key1")
 const JsUser = {
     name: "Prathamesh",
     age: 20,
     "number" : 10,
+    mySym: "mykey1",
     location: "Pune",
     email:"prathameshsonawane@google.com",
     isLoggedIn : false,
@@ -18,4 +20,8 @@ const JsUser = {
 
 console.log(JsUser["name", "email"]);
 console.log(JsUser.number);
+console.log(JsUser.mySym);
+console.log(typeof JsUser.mySym);
+
+
 
