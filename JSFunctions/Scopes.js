0e1defcbfefb036abffc6d5f7a6f238f34da740f
Prathamesh Sonawane <97780818+Prathamesh4417-HP@users.noreply.github.com>
2023-11-02 06:31:25 +0000

    function

--- JSFunctions/Scopes.js
+++ JSFunctions/Scopes.js
@@ -9,3 +9,79 @@ if(true){
 
 
 console.log(a);
+
+// nested scope
+
+function one (){
+  const username = "Stricas"
+
+  function two(){
+    const website = "YouTube"
+    console.log(website);
+
+    console.log(username);
+  }
+  // console.log(website);
+ 
+  // two()
+}
+
+// one()
+
+// using if else
+
+if(true){
+  const username = "Stricas"
+
+  if(username === "Stricas"){
+
+    const website = " youtube"
+    console.log(username + website);
+
+
+
+  }
+
+  // console.log(website);
+}
+
+// console.log(username);
+
+
+/////////////////////////////////////////////////////////////////
+console.log(addone(5))
+function addone(num1){
+  return num1 + 1
+}
+
+// console.log(addone(5))
+
+/////////////////////////////////////////////////////////////////
+
+// addTwo(5)    // in this type of fuction we cannot declare or execute function before function scope
+const addTwo = function(num1){
+  return num1 + 2
+}
+
+console.log(addTwo(5))
+
+
+
+
+
+
+
+// function myFunc(theObject) {
+//     theObject.make = "Toyota";
+//   }
+  
+//   const mycar = {
+//     make: "Honda",
+//     model: "Accord",
+//     year: 1998,
+//   };
+  
+//   console.log(mycar.make); // "Honda"
+//   myFunc(mycar);
+//   console.log(mycar.make); // "Toyota"
+  
---
+++ JSFunctions/arrow.js
@@ -0,0 +1,7 @@
+const user = {
+    username:"Stricas",
+    price:999,
+    welcomeMessage: function(){
+        
+    }
+}
